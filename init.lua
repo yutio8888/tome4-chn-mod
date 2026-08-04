d61c18661ef1ee75c976ae310aa364a816a570b9
yutio8888 <394558210@qq.com>
2026-08-04 18:37:58 +0800

    license: state GPL v3 in init.lua header per ToME4 addon convention, drop standalone COPYING/LICENSE

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
+-- chn-mod - Modified Chinese Translation
+-- ToME4 (Tales of Maj'Eyal) Chinese localization addon
+-- Copyright (C) 2026 yutio888
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+-- Upstream: ToME4 and official DLCs are Copyright (C) 2009-2019
+-- Nicolas Casalini "DarkGod" (darkgod@te4.org), GPL v3 or later.
+-- This addon is a derivative work released under the same license.
+
 long_name = "Modified Chinese Translation"
 short_name = "chn-mod"
 for_module = "tome"
